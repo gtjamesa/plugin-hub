09d229ec09364460711115d496daf737b8f4fb28
TicTac7x <tictac7x@gmail.com>
2023-02-20 12:03:22 +0200

    Comment out prints

--- src/main/java/tictac7x/charges/ChargesImprovedPlugin.java
+++ src/main/java/tictac7x/charges/ChargesImprovedPlugin.java
@@ -34,8 +34,6 @@ import java.util.Arrays;
 	tags = { "charges" }
 )
 public class ChargesImprovedPlugin extends Plugin {
-	private final boolean debug = false;
-
 	@Inject
 	private Client client;
 
@@ -151,82 +149,74 @@ public class ChargesImprovedPlugin extends Plugin {
 	@Subscribe
 	public void onChatMessage(final ChatMessage event) {
 		Arrays.stream(infoboxes_charged_items).forEach(infobox -> infobox.onChatMessage(event));
-		if (debug) {
-			System.out.println("MESSAGE | " +
-				"type: " + event.getType().name() +
-				", message: " + event.getMessage().replaceAll("</?col.*?>", "") +
-				", sender: " + event.getSender()
-			);
-		}
+//		System.out.println("MESSAGE | " +
+//			"type: " + event.getType().name() +
+//			", message: " + event.getMessage().replaceAll("</?col.*?>", "") +
+//			", sender: " + event.getSender()
+//		);
 	}
 
 	@Subscribe
 	public void onAnimationChanged(final AnimationChanged event) {
 		Arrays.stream(infoboxes_charged_items).forEach(infobox -> infobox.onAnimationChanged(event));
-		if (debug && event.getActor() == client.getLocalPlayer()) {
-			System.out.println("ANIMATION | " +
-				"id: " + event.getActor().getAnimation()
-			);
-		}
+//		if (event.getActor() == client.getLocalPlayer()) {
+//			System.out.println("ANIMATION | " +
+//				"id: " + event.getActor().getAnimation()
+//			);
+//		}
 	}
 
 	@Subscribe
 	public void onGraphicChanged(final GraphicChanged event) {
 		Arrays.stream(infoboxes_charged_items).forEach(infobox -> infobox.onGraphicChanged(event));
-		if (debug && event.getActor() == client.getLocalPlayer()) {
-			System.out.println("GRAPHIC | " +
-				"id: " + event.getActor().getGraphic()
-			);
-		}
+//		if (event.getActor() == client.getLocalPlayer()) {
+//			System.out.println("GRAPHIC | " +
+//				"id: " + event.getActor().getGraphic()
+//			);
+//		}
 	}
 
 	@Subscribe
 	public void onConfigChanged(final ConfigChanged event) {
 		Arrays.stream(infoboxes_charged_items).forEach(infobox -> infobox.onConfigChanged(event));
-		if (debug && event.getGroup().equals(ChargesImprovedConfig.group)) {
-			System.out.println("CONFIG | " +
-				"key: " + event.getKey() +
-				", old value: " + event.getOldValue() +
-				", new value: " + event.getNewValue()
-			);
-		}
+//		if (event.getGroup().equals(ChargesImprovedConfig.group)) {
+//			System.out.println("CONFIG | " +
+//				"key: " + event.getKey() +
+//				", old value: " + event.getOldValue() +
+//				", new value: " + event.getNewValue()
+//			);
+//		}
 	}
 
 	@Subscribe
 	public void onHitsplatApplied(final HitsplatApplied event) {
 		Arrays.stream(infoboxes_charged_items).forEach(infobox -> infobox.onHitsplatApplied(event));
-		if (debug) {
-			System.out.println("HITSPLAT | " +
-				"actor: " + (event.getActor() == client.getLocalPlayer() ? "self" : "enemy") +
-				", type: " + event.getHitsplat().getHitsplatType() +
-				", amount:" + event.getHitsplat().getAmount() +
-				", others = " + event.getHitsplat().isOthers() +
-				", mine = " + event.getHitsplat().isMine()
-			);
-		}
+//		System.out.println("HITSPLAT | " +
+//			"actor: " + (event.getActor() == client.getLocalPlayer() ? "self" : "enemy") +
+//			", type: " + event.getHitsplat().getHitsplatType() +
+//			", amount:" + event.getHitsplat().getAmount() +
+//			", others = " + event.getHitsplat().isOthers() +
+//			", mine = " + event.getHitsplat().isMine()
+//		);
 	}
 
 	@Subscribe
 	public void onWidgetLoaded(final WidgetLoaded event) {
 		Arrays.stream(infoboxes_charged_items).forEach(infobox -> infobox.onWidgetLoaded(event));
-		if (debug) {
-			System.out.println("WIDGET | " +
-				"group: " + event.getGroupId()
-			);
-		}
+//		System.out.println("WIDGET | " +
+//			"group: " + event.getGroupId()
+//		);
 	}
 
 	@Subscribe
 	public void onMenuOptionClicked(final MenuOptionClicked event) {
 		Arrays.stream(infoboxes_charged_items).forEach(infobox -> infobox.onMenuOptionClicked(event));
-		if (debug) {
-			System.out.println("OPTION | " +
-				"option: " + event.getMenuOption() +
-				", target: " + event.getMenuTarget() +
-				", action name: " + event.getMenuAction().name() +
-				", action id: " + event.getMenuAction().getId()
-			);
-		}
+//		System.out.println("OPTION | " +
+//			"option: " + event.getMenuOption() +
+//			", target: " + event.getMenuTarget() +
+//			", action name: " + event.getMenuAction().name() +
+//			", action id: " + event.getMenuAction().getId()
+//		);
 	}
 
 	public static String getChargesMinified(final int charges) {
